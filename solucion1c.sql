5efe200c63e6f000eb6567cb4a797b8a0bed7174
Lucas Esteban Muñoz Montes <luemunozmo@unal.edu.co>
2018-09-12 14:34:06 -0500

    corrección, aun no funciona

--- solucion1c.sql
+++ solucion1c.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE TRIGGER suc_abuela
 BEFORE DELETE ON sucursal
 FOR EACH ROW
 BEGIN
-	FOR suc IN (SELECT * FROM sucursal WHERE sucpadre = :OLD.sucursal) LOOP
+	FOR suc IN (SELECT * FROM sucursal WHERE sucpadre = :OLD.codsuc) LOOP
 		UPDATE sucursal
-		SET sucpadre = :OLD.padre
+		SET sucpadre = :OLD.sucpadre
 		WHERE codsuc = suc.codsuc;
 	END LOOP;
 END;
