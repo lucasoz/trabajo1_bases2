cc2fff14376069ed33dbbe74df6e3d7b76004c33
Lucas Esteban Muñoz Montes <luemunozmo@unal.edu.co>
2018-09-12 14:38:04 -0500

    ya da

--- solucion1c.sql
+++ solucion1c.sql
@@ -1,11 +1,14 @@
 CREATE OR REPLACE TRIGGER suc_abuela
 BEFORE DELETE ON sucursal
 FOR EACH ROW
+declare
+pragma autonomous_transaction;
 BEGIN
 	FOR suc IN (SELECT * FROM sucursal WHERE sucpadre = :OLD.codsuc) LOOP
 		UPDATE sucursal
 		SET sucpadre = :OLD.sucpadre
 		WHERE codsuc = suc.codsuc;
+		commit;
 	END LOOP;
 END;
 /
