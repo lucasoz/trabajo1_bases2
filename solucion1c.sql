78301d50188922109e5d704f9365d22251e077c3
AlejoH77 <alherrerapa@unal.edu.co>
2018-10-18 14:12:48 -0500

    Listo este punto

--- solucion1c.sql
+++ solucion1c.sql
@@ -2,14 +2,31 @@ CREATE OR REPLACE TRIGGER suc_abuela
 BEFORE DELETE ON sucursal
 FOR EACH ROW
 DECLARE
+	sucp sucursal.codsuc%TYPE;
 	PRAGMA AUTONOMOUS_TRANSACTION;
 BEGIN
-	FOR suc IN (SELECT * FROM sucursal WHERE sucpadre = :OLD.codsuc) LOOP
-		UPDATE sucursal
-		SET sucpadre = :OLD.sucpadre
-		WHERE codsuc = suc.codsuc;
-		commit;
-	END LOOP;
-	COMMIT;
+	sucp := :OLD.sucpadre;
+	IF sucp IS NULL THEN
+		DBMS_OUTPUT.PUT_LINE('intentaste borrar la raiz');
+		RAISE_APPLICATION_ERROR(-20505,'¡No puedes eliminar la sucursal raiz!');
+	ELSE
+		DBMS_OUTPUT.PUT_LINE(sucp || ' la suc padre es esa');
+		FOR suc IN (SELECT * FROM sucursal WHERE sucpadre = :OLD.codsuc) LOOP
+			UPDATE sucursal
+			SET sucpadre = :OLD.sucpadre
+			WHERE codsuc = suc.codsuc;
+			commit;
+		END LOOP;
+		COMMIT;
+	END IF;
+END;
+/
+
+--Este bloque es para probar--
+BEGIN
+DELETE FROM sucursal WHERE codsuc = 1;
+EXCEPTION
+WHEN OTHERS THEN
+DBMS_OUTPUT.PUT_LINE(SQLCODE || SQLERRM);
 END;
 /
